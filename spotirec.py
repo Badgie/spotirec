95d5f30640c39ae7ebecbefe175a517ec5765b79
Badgy <jonaskhansen@gmail.com>
2019-11-20 15:11:42 +0100

    add some comments

--- spotirec.py
+++ spotirec.py
@@ -21,13 +21,17 @@ tune_prefix = ['max', 'min', 'target']
 tune_attr = ['acousticness', 'danceability', 'duration_ms', 'energy', 'instrumentalness', 'key', 'liveness',
              'loudness', 'mode', 'popularity', 'speechiness', 'tempo', 'time_signature', 'valence', 'popularity']
 
+# OAuth handler
 sp_oauth = oauth2.SpotifyOAuth(client_id, client_secret, redirect_uri, scopes=scope, cache=cache)
 
+# Argument parser
 parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
                                  epilog="""passing no optional arguments defaults to basing recommendations off the user\'s top genres
 spotirec is released under GPL-3.0 and comes with ABSOLUTELY NO WARRANTY, for details read LICENSE""")
 parser.add_argument('-l', metavar='limit', nargs=1, type=int, choices=range(1, 101),
                     help='amount of tracks to add (default: 20, max: 100)')
+
+# Create mutually exclusive group for recommendation types to ensure only one is given
 mutex_group = parser.add_mutually_exclusive_group()
 mutex_group.add_argument('-a', action='store_true', help='base recommendations on your top artists')
 mutex_group.add_argument('-t', action='store_true', help='base recommendations on your top tracks')
@@ -37,15 +41,18 @@ mutex_group.add_argument('-gc', action='store_true', help='base recommendations 
 parser.add_argument('-b', metavar='uri', nargs='+', type=str, help='blacklist track or artist uri(s)')
 parser.add_argument('--tune', metavar='attr', nargs='+', type=str, help='specify tunable attribute(s)')
 
+# Ensure config dir and blacklist file exists
 if not os.path.isdir(f'{Path.home()}/.config/spotirec'):
     os.makedirs(f'{Path.home()}/.config/spotirec')
-
 if not os.path.exists(blacklist_path):
     f = open(blacklist_path, 'w')
     f.close()
 
 
 class Recommendation:
+    """
+    Recommendation object
+    """
     def __init__(self, t=time.localtime()):
         self.limit = 20
         self.created_at = time.ctime(time.time())
@@ -57,6 +64,11 @@ class Recommendation:
         self.playlist_name = f'Spotirec-{t.tm_mday}-{t.tm_mon}-{t.tm_year}'
 
     def playlist_description(self) -> str:
+        """
+        Create playlist description string to be insterted into playlist. Description contains
+        date and time of creation, recommendation method, and seed.
+        :return: description string
+        """
         desc = f'Created by Spotirec - {self.created_at} - based on {self.based_on} - seed: '
         if 'tracks' in self.seed_type:
             seeds = ' | '.join(str(f'{x["name"]} - {", ".join(str(y) for y in x["artists"])}')
@@ -67,15 +79,27 @@ class Recommendation:
             return f'{desc}{seeds}'
 
     def update_limit(self, limit: int):
+        """
+        Update playlist limit as object field and in request parameters.
+        :param limit: user-defined playlist limit
+        """
         self.limit = limit
         self.rec_params['limit'] = str(self.limit)
 
     def print_selection(self):
+        """
+        Print seed selection into terminal.
+        """
         print('Selection:')
         for x in self.seed_info:
             print(f'\t{self.seed_info[x]}')
 
     def add_seed_info(self, data_dict=None, data_string=None):
+        """
+        Add info about a single seed to the object fields.
+        :param data_dict: seed info as a dict if seed is artist or track
+        :param data_string: seed info as a string if seed is genre
+        """
         if 'genres' in self.seed_type:
             self.seed_info[len(self.seed_info)] = {'name': data_string}
         else:
@@ -87,6 +111,9 @@ class Recommendation:
                     self.seed_info[len(self.seed_info) - 1]['artists'].append(x['name'])
 
     def create_seed(self):
+        """
+        Construct seed string to use in request and add to object field.
+        """
         if 'genres' in self.seed_type:
             self.seed = ','.join(str(x['name']) for x in self.seed_info.values())
         else:
@@ -95,12 +122,22 @@ class Recommendation:
 
 
 def authorize():
+    """
+    Open redirect URL in browser, and host http server on localhost.
+    Function index() will be routed on said http server.
+    """
     webbrowser.open(redirect_uri)
     run(host='', port=port)
 
 
 @route('/')
 def index() -> str:
+    """
+    This function is routed to http server hosted on localhost.
+    Retrieve code from redirect URL once authorization is complete and retrieve token from API.
+    :return: success confirmation if access token is found
+    :return: link to authorization if access token wasn't found
+    """
     access_token = ""
     url = request.url
     code = sp_oauth.parse_response_code(url)
@@ -115,6 +152,10 @@ def index() -> str:
 
 
 def get_token() -> str:
+    """
+    Retrieve access token from OAuth handler. Try to authorize and exit if credentials don't exist.
+    :return: access token, if present
+    """
     creds = sp_oauth.get_credentials()
     if creds:
         return creds['access_token']
@@ -124,12 +165,22 @@ def get_token() -> str:
 
 
 def get_top_list(list_type: str, top_limit: int) -> json:
+    """
+    Retrieve list of top artists of tracks from user's profile.
+    :param list_type: type of list to retrieve; 'artists' or 'tracks'
+    :param top_limit: amount of entries to retrieve; min 1, max 50
+    :return: top list as json object
+    """
     params = {'limit': top_limit}
     response = requests.get(f'{url_base}/me/top/{list_type}', headers=headers, params=params)
     return json.loads(response.content.decode('utf-8'))
 
 
 def add_top_genres_seed():
+    """
+    Extract genres from user's top 50 artists and sort them from high to low.
+    Add top 5 genres to recommendation object seed info.
+    """
     data = get_top_list('artists', 50)
     genres = {}
     for x in data['items']:
@@ -144,11 +195,19 @@ def add_top_genres_seed():
 
 
 def get_user_id() -> str:
+    """
+    Retrieve user ID from API.
+    :return: user ID as a string
+    """
     response = requests.get(f'{url_base}/me', headers=headers)
     return json.loads(response.content.decode('utf-8'))['id']
 
 
 def create_playlist() -> str:
+    """
+    Creates playlist on user's account.
+    :return: ID of the newly created playlist
+    """
     data = {'name': rec.playlist_name,
             'description': rec.playlist_description()}
     print('Creating playlist')
@@ -157,17 +216,31 @@ def create_playlist() -> str:
 
 
 def add_to_playlist(tracks: list, playlist: str):
+    """
+    Add tracks to playlist.
+    :param tracks: list of track URIs
+    :param playlist: playlist ID
+    """
     data = {'uris': tracks}
     print('Adding tracks to playlist')
     requests.post(f'{url_base}/playlists/{playlist}/tracks', headers=headers, json=data)
 
 
 def get_recommendations() -> json:
+    """
+    Retrieve recommendations from API.
+    :return: recommendations as json object
+    """
     response = requests.get(f'{url_base}/recommendations', params=rec.rec_params, headers=headers)
     return json.loads(response.content.decode('utf-8'))
 
 
 def filter_recommendations(data: json) -> list:
+    """
+    Filter blacklisted artists and tracks from recommendations.
+    :param data: recommendations as json object.
+    :return: list of eligible track URIs
+    """
     tracks = []
     with open(blacklist_path, 'r+') as file:
         try:
@@ -186,6 +259,11 @@ def filter_recommendations(data: json) -> list:
 
 
 def recommend():
+    """
+    Main function for recommendations. Retrieves recommendations and tops up list if any tracks
+    were removed by the blacklist filter. Playlist is created and tracks are added. Seed info
+    is printed to terminal.
+    """
     print('Getting recommendations')
     rec.create_seed()
     tracks = filter_recommendations(get_recommendations())
@@ -204,6 +282,12 @@ def recommend():
 
 
 def print_choices(data: list) -> str:
+    """
+    Used for custom seed creation. All valid choices are printed to terminal and user is prompted
+    to select. If the seed type is genres, seeds are simply added to the recommendations object.
+    :param data: valid choices as a list of names
+    :return: user input, if seed type is artists or tracks
+    """
     line = ""
     for x in range(0, round(len(data)), 3):
         try:
@@ -226,11 +310,21 @@ def print_choices(data: list) -> str:
 
 
 def add_top_seed_info(data: json):
+    """
+    Add artists or tracks to seed info. This function should only be used in recommendations
+    that use top 5 tracks or artists.
+    :param data: artists or tracks as a json object
+    """
     for x in data['items']:
         rec.add_seed_info(data_dict=x)
 
 
 def add_custom_seed_info(data: json):
+    """
+    Construct dict only containing artist or track names and IDs and prompt for selection.
+    Seeds are added to recommendation object.
+    :param data: valid choices as json object
+    """
     choices = {}
     for x in data['items']:
         choices[x['name']] = x['id']
@@ -240,6 +334,9 @@ def add_custom_seed_info(data: json):
 
 
 def parse():
+    """
+    Parse arguments
+    """
     args = parser.parse_args()
     if args.b:
         with open(blacklist_path, 'r') as file:
