9c772e337a8e4e2f250aefb0923593fd0c85a4b2
Badgy <jonaskhansen@gmail.com>
2020-05-14 16:17:28 +0200

    revert test runner settings

--- tests/lib/__init__.py
+++ tests/lib/__init__.py
@@ -5,4 +5,4 @@ ordered, compare = lib.order_handler()
 
 # setup runner with low verbosity, and no variable printing on fail/error, and custom results class
 # for verbose output and variable printing, set verbosity=5 and tb_locals=True
-runner = SpotirecTestRunner(verbosity=5, tb_locals=True, resultclass=SpotirecTestResults)
+runner = SpotirecTestRunner(verbosity=0, tb_locals=False, resultclass=SpotirecTestResults)
